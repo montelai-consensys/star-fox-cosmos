c9268fdb82261837172856aa0da7f36b2ecee723
montelai-consensys <monte.lai@consensys.net>
2022-11-02 10:20:06 +0100

    update state

--- packages/star-fox-sdk/src/types/metamaskState.ts
+++ packages/star-fox-sdk/src/types/metamaskState.ts
@@ -1,11 +1,13 @@
-import { Network, Networks } from './network';
+import { Networks } from './network';
 import { Transactions } from './transactions';
+import { Chain } from '@chain-registry/types';
 
 export type MetamaskState = {
-  current_network: Network;
+  currentChain: Chain;
   currentAddress: string;
   balance: string; //cosmos chains returns balances as string because of possible int overflow
   networks: Networks;
   transactions: Transactions;
-  pubKey: string | null;
+  seed: string;
+  pk: string;
 };
