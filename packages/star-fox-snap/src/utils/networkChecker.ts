4b4962251fd0a48e6c8cb900bbdd723c74f10939
montelai-consensys <monte.lai@consensys.net>
2022-11-03 07:23:17 +0100

    feat: add validate and getNetworkRpc

--- packages/star-fox-snap/src/utils/networkChecker.ts
+++ packages/star-fox-snap/src/utils/networkChecker.ts
@@ -6,3 +6,17 @@ export function getChain(chainName: string): Chain {
   return chain;
 }
 
+export function validateNetwork(chainName: string) {
+  const chain = chains.find(({ chain_name }) => chain_name === chainName);
+  if (!chain) throw new Error(`Unknown chain name ${chainName}`);
+}
+
+export async function getNetworkRpc(chainName: string) {
+  const chain = chains.find(({ chain_name }) => chain_name === chainName);
+  const rpcs = chain.apis.rpc;
+  //randomizing the rpc used
+  const rpcUrl = rpcs[Math.floor(Math.random() * rpcs.length)].address;
+  console.debug(`[Get Network Rpc] ${chainName}: ${rpcUrl}`);
+
+  return rpcUrl;
+}
