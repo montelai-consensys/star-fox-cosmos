80f1800969f692e9b2531fce91de6c8c7a38a56f
montelai-consensys <monte.lai@consensys.net>
2022-11-06 21:38:47 +0100

    fix: update MetamaskState type

--- packages/star-fox-sdk/src/types/metamaskState.ts
+++ packages/star-fox-sdk/src/types/metamaskState.ts
@@ -1,12 +1,46 @@
-import { Transactions } from './transactions';
-import { Chain } from '@chain-registry/types';
+import { SnapTransactions } from './transactions';
+import { Asset } from '@chain-registry/types';
 
-export type MetamaskState = {
-  currentChain: Chain;
+export interface CurrentChain {
+  currentChain: FormattedChainWithAddress;
   currentAddress: string;
   balance: string; //cosmos chains returns balances as string because of possible int overflow
-  networks: Record<string, Chain>;
-  transactions: Transactions;
-  //seed: string;
-  pk: string;
-};
+}
+
+export interface MetamaskState extends CurrentChain {
+  networks: SnapNetworks;
+  balances: SnapBalances;
+  transactions: SnapTransactions;
+  publicKey: string;
+}
+
+export interface ChainFee {
+  denom: string;
+  fixed_min_gas_price?: number;
+  low_gas_price?: number;
+  average_gas_price?: number;
+  high_gas_price?: number;
+}
+
+export interface FormattedChain {
+  chain_name: string;
+  chain_id: string;
+  website: string;
+  network_type: string;
+  bech32_prefix: string;
+  rpc: Array<string>;
+  fees: ChainFee;
+  denom: string;
+}
+
+export interface FormattedChainWithAddress extends FormattedChain {
+  address: string;
+}
+
+export interface AssetWithBalance extends Asset {
+  balance: string | number;
+}
+
+export type SnapNetworks = Record<string, FormattedChainWithAddress>;
+export type SnapBalances = Record<string, SnapSingleNetworkBalances>;
+export type SnapSingleNetworkBalances = Record<string, AssetWithBalance>;
