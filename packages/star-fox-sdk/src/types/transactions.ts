07ed6c40b573c154722504719a09f6cd8c09dd57
montelai-consensys <monte.lai@consensys.net>
2022-11-06 21:41:41 +0100

    feat: add tx types

--- packages/star-fox-sdk/src/types/transactions.ts
+++ packages/star-fox-sdk/src/types/transactions.ts
@@ -1,4 +1,6 @@
-export type Transactions = Record<string, Array<Transaction>>;
+import { StdFee, StdSignature } from '@cosmjs/amino';
+
+export type SnapTransactions = Record<string, Array<Transaction>>;
 
 export interface Transaction {
   txHash: string;
@@ -19,3 +21,52 @@ export interface TransactionData {
 }
 
 export interface Log {}
+
+export interface SignAminoDocDto {
+  chain_id: string;
+  account_number: string;
+  sequence: string;
+  fee: StdFee;
+  msgs: Array<AminoMsgDto>;
+  memo: string;
+}
+
+export interface AminoMsgDto {
+  type: string;
+  value: string; //utin8array
+}
+
+export interface SignDocDto {
+  bodyBytes: string; //uint8array
+  chainId: string;
+  accountNumber: string; // Long
+  authInfoBytes: string; //uint8array
+}
+
+export interface SignDirectPayload {
+  signDocDto: SignDocDto;
+  signerAddress: string;
+}
+
+export interface SignAminoPayload {
+  signDocDto: SignAminoDocDto;
+  signerAddress: string;
+}
+
+export interface SignatureDto {
+  signature: string;
+  pubKey: {
+    type: string;
+    value: string; // Uint8Array as string
+  };
+}
+
+export interface SignAminoResponseDto {
+  signDocDto: SignAminoDocDto;
+  signature: SignatureDto;
+}
+
+export interface SignDirectResponseDto {
+  signDocDto: SignDocDto;
+  signature: SignatureDto;
+}
