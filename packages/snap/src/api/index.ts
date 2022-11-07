ce7f6a0ec221323166821363766e9d62b23a2f0e
montelai-consensys <monte.lai@consensys.net>
2022-11-07 21:14:29 +0100

    update snap requestHandler

---
+++ packages/snap/src/api/index.ts
@@ -0,0 +1,6 @@
+export * from './getAllIBCAssets';
+export * from './getBalance';
+export * from './getSigningClient';
+export * from './ibc-transfer';
+export * from './transfer';
+export * from './changeNetwork';
---
+++ packages/snap/src/index.ts
@@ -0,0 +1,105 @@
+import { OnRpcRequestHandler, SnapProvider } from '@metamask/snap-types';
+import { getBalance, sendTransfer, changeNetwork } from './api';
+import {
+  BalanceQuery,
+  ChangeNetworkQuery,
+  MetamaskState,
+  SignAminoPayload,
+  SignDirectPayload,
+} from '../../sdk/src/index';
+import {
+  showConfirmationDialog,
+  ConfirmationDialogContent,
+} from './utils/confirmation';
+import { initializeAccount } from './cosmos/initializeAccount';
+import { RequestParam } from './types';
+import { signDirect } from './api/signDirect';
+import { getCurrentState } from './utils/getCurrentState';
+
+declare const wallet: SnapProvider;
+
+export const onRpcRequest: OnRpcRequestHandler = async ({
+  origin,
+  request,
+}) => {
+  const requestParams = request.params as unknown as RequestParam;
+  let state: MetamaskState = (await wallet.request({
+    method: 'snap_manageState',
+    params: ['get'],
+  })) as MetamaskState;
+
+  console.log(`[Origin] ${origin}`);
+  console.debug(`Current State`, state);
+
+  if (!state) {
+    console.debug(`State not found, reinitializing`);
+    state = await initializeAccount(wallet);
+    console.log(state);
+  }
+
+  switch (request.method) {
+    case 'hello':
+      const message: ConfirmationDialogContent = {
+        prompt: 'Hello Ping',
+        description: 'This is a ping message',
+      };
+      return await showConfirmationDialog(wallet, message);
+
+    // IMPLEMENTATION OF OFFLINE DIRECT SIGNER AND OFFLINE AMINO SIGNER
+    case 'getAccount':
+      return {
+        address: state.currentAddress,
+        algo: 'secp256k1',
+        pubkey: state.publicKey,
+      };
+    case 'signDirect':
+      const response = await signDirect(
+        wallet,
+        state,
+        request.params as unknown as SignDirectPayload
+      );
+      return response;
+    case 'signAmino':
+      return;
+
+    // Convenience Methods of snap
+    case 'getCurrentNetwork':
+      return getCurrentState(state);
+    case 'addNetwork':
+      return;
+    case 'changeNetwork':
+      const [updatedState, networkChangePayload] = await changeNetwork(
+        wallet,
+        state,
+        request.params as unknown as ChangeNetworkQuery
+      );
+      return networkChangePayload;
+    case 'updateRpc':
+      return;
+    case 'getAddress':
+      const pk = state.currentAddress;
+      return pk;
+    case 'getBalance':
+      return await getBalance(request.params as unknown as BalanceQuery);
+    case 'transfer':
+      const transferResult = await sendTransfer(wallet, state, request.params);
+      return transferResult;
+    case 'ibc_transfer':
+      //return await sendIBCTransfer(state, client, request.params);
+      return;
+    case 'signTransactionAndBroadcast':
+      return;
+    case 'getTransactionsByNetwork':
+      return;
+    case 'user_getAllBalancesByNetwork':
+      return;
+    case 'reset':
+      console.log('Resetting state');
+      return await wallet.request({
+        method: 'snap_manageState',
+        params: ['clear'],
+      });
+    default:
+      throw new Error('Method not found.');
+  }
+};
