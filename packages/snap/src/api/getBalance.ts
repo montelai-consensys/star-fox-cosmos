11f1c464fbc145332f4bea9d3bd5ede9277690af
montelai-consensys <monte.lai@consensys.net>
2022-11-07 21:09:36 +0100

    snap add getBalance

---
+++ packages/snap/src/api/getBalance.ts
@@ -0,0 +1,33 @@
+import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
+import { createProtobufRpcClient, QueryClient } from '@cosmjs/stargate';
+import { QueryClientImpl } from 'cosmjs-types/cosmos/bank/v1beta1/query';
+import { getAllIBCAssetsByChain } from './getAllIBCAssets';
+import {
+  BalanceQueryResponse,
+  BalanceQuery,
+  getNetworkRpc,
+} from '../../../sdk/src/index';
+
+export async function getBalance(
+  balanceQuery: BalanceQuery
+): Promise<BalanceQueryResponse> {
+  console.debug('[GetBalance] Query', balanceQuery);
+  const { address, denom, chainName }: BalanceQuery = balanceQuery;
+  console.log('IBC Assets', getAllIBCAssetsByChain(chainName));
+  const rpcUrl = await getNetworkRpc(chainName);
+
+  //create read only client
+  const tendermint = await Tendermint34Client.connect(rpcUrl);
+  const queryClient = new QueryClient(tendermint);
+  const rpcClient = createProtobufRpcClient(queryClient);
+  const bankQueryService = new QueryClientImpl(rpcClient);
+
+  const { balance } = await bankQueryService.Balance({
+    address,
+    denom,
+  });
+
+  console.debug(`Returned balance: ${balance.amount} ${balance.denom}`);
+
+  return balance;
+}
