ddd8ae1357c257f8e7f25d72850286f5664f06ed
montelai-consensys <monte.lai@consensys.net>
2022-11-14 13:04:26 +0800

    snap update snapCommand and add logs

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,11 +1,18 @@
 import { OnRpcRequestHandler, SnapProvider } from '@metamask/snap-types';
-import { getBalance, sendTransfer, changeNetwork } from './api';
+import {
+  getBalance,
+  sendTransfer,
+  changeNetwork,
+  sendIBCTransfer,
+} from './api';
 import {
   BalanceQuery,
   ChangeNetworkQuery,
+  IBCTransferPayload,
   MetamaskState,
   SignAminoPayload,
   SignDirectPayload,
+  TransferPayload,
 } from '@consensys/star-fox-sdk';
 //} from '../../sdk/src/index';
 import {
@@ -13,9 +20,9 @@ import {
   ConfirmationDialogContent,
 } from './utils/confirmation';
 import { initializeAccount } from './cosmos/initializeAccount';
-import { RequestParam } from './types';
 import { signDirect } from './api/signDirect';
 import { getCurrentState } from './utils/getCurrentState';
+import { signAmino } from './api/signAmino';
 
 declare const wallet: SnapProvider;
 
@@ -23,7 +30,6 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
   origin,
   request,
 }) => {
-  const requestParams = request.params as unknown as RequestParam;
   let state: MetamaskState = (await wallet.request({
     method: 'snap_manageState',
     params: ['get'],
@@ -46,59 +52,97 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
       return await showConfirmationDialog(wallet, message);
 
     // IMPLEMENTATION OF OFFLINE DIRECT SIGNER AND OFFLINE AMINO SIGNER
-    case 'getAccount':
+    case 'starFoxSnap_getAccount':
+      console.debug('[getAccount] Starting');
       return {
         address: state.currentAddress,
         algo: 'secp256k1',
         pubkey: state.publicKey,
       };
-    case 'signDirect':
+    case 'starFoxSnap_signDirect':
+      console.debug('[signDirect] Starting');
+
       const response = await signDirect(
         wallet,
         state,
         request.params as unknown as SignDirectPayload
       );
       return response;
-    case 'signAmino':
+    case 'starFoxSnap_signAmino':
+      console.debug('[signAmino] Starting');
+      const response = await signAmino(
+        wallet,
+        state,
+        request.params as SignAminoPayload
+      );
       return;
 
     // Convenience Methods of snap
-    case 'snap_getChainsAndBalances':
+    case 'starFoxSnap_getChainsAndBalances':
+      console.debug('[getChainsAndBalances] Starting');
+
       return getCurrentState(state);
-    case 'snap_getCurrentNetwork':
+    case 'starFoxSnap_getCurrentNetwork':
+      console.debug('[getCurrentNetwork] Starting');
+
       return {
         currentChain: state.currentChain,
         currentAddress: state.currentAddress,
       };
-    case 'addNetwork':
+    case 'starFoxSnap_addNetwork':
+      console.debug('[addNetwork] Starting');
+
       return;
-    case 'changeNetwork':
-      const [updatedState, networkChangePayload] = await changeNetwork(
+    case 'starFoxSnap_changeNetwork':
+      console.debug('[changeNetwork] Starting');
+
+      const [_, networkChangePayload] = await changeNetwork(
         wallet,
         state,
         request.params as unknown as ChangeNetworkQuery
       );
       return networkChangePayload;
-    case 'updateRpc':
+    case 'starFoxSnap_updateRpc':
+      console.debug('[updateRpc] Starting');
+
       return;
-    case 'getAddress':
+    case 'starFoxSnap_getAddress':
+      console.debug('[getAddress] Starting');
+
       const pk = state.currentAddress;
       return pk;
-    case 'getBalance':
+    case 'starFoxSnap_getBalance':
+      console.debug('[getBalance] Starting');
+
       return await getBalance(request.params as unknown as BalanceQuery);
-    case 'transfer':
-      const transferResult = await sendTransfer(wallet, state, request.params);
+    case 'starFoxSnap_transfer':
+      console.debug('[transfer] Starting');
+
+      const transferResult = await sendTransfer(
+        wallet,
+        state,
+        request.params as TransferPayload
+      );
       return transferResult;
-    case 'ibc_transfer':
-      //return await sendIBCTransfer(state, client, request.params);
+    case 'starFoxSnap_ibcTransfer':
+      console.debug('[ibcTransfer] Starting');
+
+      return await sendIBCTransfer(
+        wallet,
+        state,
+        request.params as IBCTransferPayload
+      );
+    case 'starFoxSnap_signTransactionAndBroadcast':
+      console.debug('[signTransactionandBroadcast] Starting');
+
       return;
-    case 'signTransactionAndBroadcast':
+    case 'starFoxSnap_getTransactionsByNetwork':
+      console.debug('[getTransactionsByNetwork] Starting');
+
       return;
-    case 'getTransactionsByNetwork':
-      return;
-    case 'user_getAllBalancesByNetwork':
-      return;
-    case 'reset':
+    case 'starFoxSnap_reset':
+      console.debug('[reset] Starting');
+
       console.log('Resetting state');
       return await wallet.request({
         method: 'snap_manageState',
