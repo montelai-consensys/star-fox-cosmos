3310b568704aaa60fc8f56c0c0b8792f7939b17f
montelai-consensys <monte.lai@consensys.net>
2022-11-24 14:12:36 +0800

    feat: frontend show all asset button

--- packages/frontend/components/assets/assets.tsx
+++ packages/frontend/components/assets/assets.tsx
@@ -1,16 +1,30 @@
-import { Flex } from '@chakra-ui/react';
-import { selectBalances, selectChains } from 'packages/frontend/store/slices/chain.slice';
-import { selectSnapState } from 'packages/frontend/store/slices/snap.slice';
+import { Flex, HStack, Switch, Text } from '@chakra-ui/react';
+import { selectBalances} from 'packages/frontend/store/slices/chain.slice';
 import { useAppSelector } from 'packages/frontend/store/store';
+import {useState} from 'react';
 import { Asset } from './asset';
+import {BigNumber} from 'bignumber.js'
 
 export const AssetsPanel = ({ chainId }) => {
     const balances = useAppSelector(selectBalances);
-    const chains = useAppSelector(selectChains)
+    const [showAllAssets, setShowAllAssetsToggle] = useState(false)
 
     return (
         <Flex direction="column" flex={1} width="100%">
-            {Object.keys(balances[chainId]).map(assetName => {
+
+            <HStack justifySelf="flex-end"><Text>Show Assets With Small Balances</Text><Switch isChecked={showAllAssets} onChange={() => setShowAllAssetsToggle(!showAllAssets)}/></HStack>
+            {showAllAssets ? Object.keys(balances[chainId]).map(assetName => {
+                return (
+                    <Asset
+                        key={assetName}
+                        chainId={chainId}
+                        imageURI={balances[chainId][assetName].imageURI}
+                        symbol={balances[chainId][assetName].symbol}
+                        decimal={balances[chainId][assetName].decimal}
+                        amount={balances[chainId][assetName].balance}
+                    />
+                );
+            }): Object.keys(balances[chainId]).filter(assetName => new BigNumber(balances[chainId][assetName].balance).gt(new BigNumber(0))).map(assetName => {
                 return (
                     <Asset
                         key={assetName}
