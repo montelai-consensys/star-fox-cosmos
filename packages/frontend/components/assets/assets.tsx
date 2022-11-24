4e28d8c7e7632a1797454443565a6ebe224ca758
montelai-consensys <monte.lai@consensys.net>
2022-11-24 16:04:51 +0800

    fix: frontend default show all assets

--- packages/frontend/components/assets/assets.tsx
+++ packages/frontend/components/assets/assets.tsx
@@ -7,12 +7,12 @@ import {BigNumber} from 'bignumber.js'
 
 export const AssetsPanel = ({ chainId }) => {
     const balances = useAppSelector(selectBalances);
-    const [showAllAssets, setShowAllAssetsToggle] = useState(false)
+    const [showAllAssets, setShowAllAssetsToggle] = useState(true)
 
     return (
         <Flex direction="column" flex={1} width="100%">
 
-            <HStack justifySelf="flex-end"><Text>Show Assets With Small Balances</Text><Switch isChecked={showAllAssets} onChange={() => setShowAllAssetsToggle(!showAllAssets)}/></HStack>
+            <HStack justifySelf="flex-end" mb={5}><Text>Show Assets With Small Balances</Text><Switch isChecked={showAllAssets} onChange={() => setShowAllAssetsToggle(!showAllAssets)}/></HStack>
             {showAllAssets ? Object.keys(balances[chainId]).map(assetName => {
                 return (
                     <Asset
